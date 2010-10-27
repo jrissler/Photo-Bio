4f5c337a1ab3ac89cb9badcc270c762789e93476
jrissler <james@jamesrissler.com>
2010-10-27 17:39:04 -0400

    Initial Rails 3 app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PhotoBio::Application.config.secret_token = 'f0ff321097ba560972fed7493f6492529b01952db41985ebc3b4115a6cca8d29ddb9fe5d17f9fd54672800b9e26f736ce731545f0adb56d82b5ffd4db628cfd8'
