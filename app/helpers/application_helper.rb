2e0b5806077f009ae772eba64893c25240bf28b4
jrissler <james@jamesrissler.com>
2010-11-03 08:41:14 -0400

    Added common menu helpers to generate the menus.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,38 @@
 module ApplicationHelper
   
   def page_title
-    "Photo Bio" + controller.action_name + ": " + controller.controller_name
+    "Photo Bio " + controller.action_name + ": " + controller.controller_name
   end
+  
+  def main_nav
+    links = []
+    links << link_to("Home", root_url, :title => "Home")
+    links << link_to("Check ti", root_url, :title => "Home")
+    links << link_to("About", root_url, :title => "Home")
+    
+    menu(links)
+  end
+  
+  def menu(links)
+    content_tag :ul do
+      convert_to_list(links)
+    end
+  end
+  
+  def convert_to_list(links)
+    a = []
+    links.each do |link|
+      a << content_tag(:li, link, :class => classy(link, links) )
+    end
+    a.join('')
+  end
+  
+  def classy(link, links)
+    if link == links.first
+      return "first"
+    elsif link == links.last
+      return "last"
+    end
+  end
+
 end
